c07723807eb83d9810315a3def9c38bf61fbaade
tomoki <benzousan7@gmail.com>
2022-12-04 14:34:21 +0900

    ユーザー編集画面を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,22 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
   end
 
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "ユーザーを編集しました！"
+    else
+      render :edit
+    end
+  end
+
 
   private
   def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name,:email,:password,:password_confirmation,:profile_photo)
   end
 end
